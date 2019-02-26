14d4f1be4ca6951575705303f7447e956cc2e966
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-02-26 20:41:22 -0300

    agregando colores con chalk

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 #!/usr/bin/env node
-'use strict'
-/*module.exports = () => {
-  // ...
-};
-*/
+
 const fs =  require('fs');
 const path =  require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
 const https = require('https');
-//const fetch = require('node-fetch');
+const chalk = require('chalk');
+const fetch = require('node-fetch');
 //const _ = require('lodash');
 
 const URLpath = process.argv[2];
 
-function readDir (URLpath){
-  let promise = new Promise((res, reject) => {
-    let URLabsolute = path.resolve(URLpath);
-    let extensionFile = path.extname(URLabsolute);
-     if (extensionFile === '.md') {
-        let markdown = fs.readFileSync(URLabsolute).toString();
-        let links = markdownLinkExtractor(markdown);
-        links.forEach(function (link) { 
-          res(https.get(link, (res)=>{
-            if (res.statusCode >= 200 && res.statusCode <= 208){
-              console.log(link + ' link is OK');
-            }
-            else if (res.statusCode >= 300 && res.statusCode <= 308) {
-              console.log(link + ' Redirect');
-            }
-            else if(res.statusCode >= 400 && res.statusCode <= 451) {
-              console.log(link + ' Not Found');
-            }
-          }));
-        });
-      } else {
-        reject(new Error('wooops'));
-      }
-  });
-  return promise;
-}
 
-readDir(URLpath);
+  const readDir = (URLpath) => {
+    let promise = new Promise((res, reject) => {
+      let URLabsolute = path.resolve(URLpath);
+      let extensionFile = path.extname(URLabsolute);
+       if (extensionFile === '.md') {
+          let markdown = fs.readFileSync(URLabsolute).toString();
+          let links = markdownLinkExtractor(markdown);
+          links.forEach(function (link) { 
+              res(https.get(link, (res)=>{
+              if (res.statusCode >= 200 && res.statusCode <= 208){
+                console.log(link + chalk.green(' link is OK'));
+              }
+              else if (res.statusCode >= 300 && res.statusCode <= 308) {
+                console.log(link + chalk.blue(' Redirect'));
+              }
+              else if(res.statusCode >= 400 && res.statusCode <= 451) {
+                console.log(link + chalk.red(' Not Found'));
+              }
+            }));
+          });
+          }
+        else {
+              reject(new Error('wooops'));
+            }
+      });
+    return promise;
+    }
+
+    readDir(URLpath);
+  
+
 
