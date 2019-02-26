d11ae5024ba73745c5ad300da3817ec515f9d50a
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-02-26 15:27:20 -0300

    agregando funcion para leer archivos y extraer links

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
-
+'use strict'
 /*module.exports = () => {
   // ...
 };
 */
-const fs =  require ('fs');
-const path =  require ('path');
+const fs =  require('fs');
+const path =  require('path');
+const markdownLinkExtractor = require('markdown-link-extractor');
 const fetch = require('node-fetch');
+//const _ = require('lodash');
 
+const URLpath = process.argv[2];
 
-function readDir (dir){
+function readDir (URLpath){
+  let promise = new Promise((res, reject) => {
+    let URLabsolute = path.resolve(URLpath);
+    let extensionFile = path.extname(URLabsolute);
+     if (extensionFile === '.md') {
+        let markdown = fs.readFileSync(URLabsolute).toString();
+        let links = markdownLinkExtractor(markdown);
 
-  fs.readdir(dir, function(err, files){
-    if (err){
-      console.log(err)
-    }
-    console.log(files);
-  })
-
+        links.forEach(function (link) { 
+          res(console.log(link));
+        });
+      } else {
+        reject(new Error('wooops'));
+      }
+  });
+  return promise;
 }
-console.log(readDir("./"));
+
+readDir(URLpath);
+
