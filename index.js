86442a3a10823831fe4a051a39ddd113b34fb3f4
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-02-27 21:09:49 -0300

    busca archivos .md en directorios, cambiando http.get por fetch

--- index.js
+++ index.js
@@ -3,43 +3,48 @@
 const fs =  require('fs');
 const path =  require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
-const https = require('https');
+//const https = require('https');
 const chalk = require('chalk');
 const fetch = require('node-fetch');
 //const _ = require('lodash');
 
 const URLpath = process.argv[2];
 
-
-  const readDir = (URLpath) => {
-    let promise = new Promise((res, reject) => {
-      let URLabsolute = path.resolve(URLpath);
-      let extensionFile = path.extname(URLabsolute);
-       if (extensionFile === '.md') {
+    const readDirOrFile = (URLpath) => {
+      let promise = new Promise((res, reject) => {
+        let URLabsolute = path.resolve(URLpath);
+        let extensionFile = path.extname(URLabsolute);
+        if (fs.lstatSync(URLabsolute).isDirectory()){
+          fs.readdirSync(URLabsolute).forEach(file => { 
+            if (fs.lstatSync(URLabsolute + "/" + file).isDirectory() || path.extname(URLabsolute + "/" + file) === '.md'){
+              readDirOrFile(URLabsolute + "/" + file); 
+            } /* else {
+              console.log("soy un archivo cualquiera")
+            }  */   
+          }); 
+        }
+        else if (fs.lstatSync(URLabsolute).isFile() && extensionFile === '.md'){
           let markdown = fs.readFileSync(URLabsolute).toString();
           let links = markdownLinkExtractor(markdown);
-          links.forEach(function (link) { 
-              res(https.get(link, (res)=>{
-              if (res.statusCode >= 200 && res.statusCode <= 208){
-                console.log(link + chalk.green(' link is OK'));
-              }
-              else if (res.statusCode >= 300 && res.statusCode <= 308) {
-                console.log(link + chalk.blue(' Redirect'));
-              }
-              else if(res.statusCode >= 400 && res.statusCode <= 451) {
-                console.log(link + chalk.red(' Not Found'));
-              }
-            }));
-          });
-          }
+          links.forEach(function (link) {
+            return res(fetch(link)
+            .then(res => {
+              return console.log({"href": res.url,  "status": res.status}) 
+            })
+            .catch(error => {return error})
+          )})
+        }
         else {
-              reject(new Error('wooops'));
-            }
-      });
+          reject(new Error('ups'))
+        }
+      })
     return promise;
-    }
+  }
 
-    readDir(URLpath);
+         
+        
+    readDirOrFile(URLpath);
+
   
 
 
