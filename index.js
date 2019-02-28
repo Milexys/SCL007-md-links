785927099cbc1e333dc6151fe9eb2aea8eb208e2
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-02-28 17:06:57 -0300

    promesa devuelve arreglo de objetos

--- index.js
+++ index.js
@@ -3,47 +3,77 @@
 const fs =  require('fs');
 const path =  require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
-//const https = require('https');
 const chalk = require('chalk');
 const fetch = require('node-fetch');
-//const _ = require('lodash');
+
+
 
 const URLpath = process.argv[2];
+const validate = process.argv.indexOf("--validate");
 
     const readDirOrFile = (URLpath) => {
-      let promise = new Promise((res, reject) => {
         let URLabsolute = path.resolve(URLpath);
         let extensionFile = path.extname(URLabsolute);
         if (fs.lstatSync(URLabsolute).isDirectory()){
           fs.readdirSync(URLabsolute).forEach(file => { 
             if (fs.lstatSync(URLabsolute + "/" + file).isDirectory() || path.extname(URLabsolute + "/" + file) === '.md'){
               readDirOrFile(URLabsolute + "/" + file); 
-            } /* else {
-              console.log("soy un archivo cualquiera")
-            }  */   
+            }   
           }); 
         }
         else if (fs.lstatSync(URLabsolute).isFile() && extensionFile === '.md'){
           let markdown = fs.readFileSync(URLabsolute).toString();
           let links = markdownLinkExtractor(markdown);
-          links.forEach(function (link) {
-            return res(fetch(link)
-            .then(res => {
-              return console.log({"href": res.url,  "status": res.status}) 
-            })
-            .catch(error => {return error})
-          )})
+          makePromises(links);
         }
-        else {
-          reject(new Error('ups'))
+  } 
+
+  const makePromises = (links) => {
+    let URLabsolute = path.resolve(URLpath);
+    const arr = [];
+    for(let i = 0; i < links.length; i++){
+      const link = links[i];
+      const text = links[i].text;
+      const fetchLinks =  fetch(link)
+      .then(res => {
+        if (validate === 3){
+          const objectLinks = {
+            url: res.url,
+            text: text,
+            file: URLabsolute,
+            statusLink: res.status,
+            statusText: res.statusText,
+          };
+          return objectLinks;
+        } else{
+          const objectLinks = {
+            url: res.url,
+            text: text,
+            file: URLabsolute,
+          };
+          return objectLinks;
         }
       })
-    return promise;
+      .catch(error => {
+        const objectLinks = {
+          url: link,
+          statusLinks: "Fail",
+        };
+        return objectLinks;
+      })
+      arr.push(fetchLinks);
+    }
+    Promise.all(arr)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 
-         
-        
-    readDirOrFile(URLpath);
+  readDirOrFile(URLpath);
+
 
   
 
